bc8162c020cf730363472bed1826b5c96ddf5374
890asif <192508892+890asif@users.noreply.github.com>
2026-08-15 17:34:02 +0600

    primitive type use

---
+++ problem-2.ts
@@ -0,0 +1,27 @@
+//primitive type 
+const productDetails =(name:string, price:number, isAvailable:boolean):string =>{
+
+    return `${name} cost ${price} and is available: ${isAvailable}`
+}
+
+console.log(productDetails("Laptop", 45000, true))
+
+
+
+//non primitive type
+const productDetails = (product:{name:string, price:number, isAvailable:boolean})=>{
+
+    const {name, price, isAvailable} = product;
+
+    return `${name} cost ${price} and is available: ${isAvailable}`
+
+}
+const product:{name:string;
+    price:number;
+    isAvailable: boolean;
+} = {
+    name: "Laptop",
+    price: 45000,
+    isAvailable: true
+}
+console.log(productDetails(product))
