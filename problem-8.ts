8b01250c92c483b020ebce054f8acbaf57dd7652
890asif <192508892+890asif@users.noreply.github.com>
2026-08-22 14:58:26 +0600

    even or odd return using ternary operator

---
+++ problem-8.ts
@@ -0,0 +1,14 @@
+// type Num = number;
+// const findOddEvenNumber = (num:Num):string =>{
+//     if(num % 2 === 0){
+//         return `positive`
+//     }
+//     else{
+//         return `negetive`
+//     }
+// }
+// console.log(findOddEvenNumber(5))
+
+const num:number = 5;
+const result:string = num >= 0 ? "Positive" : "Negetive";
+console.log(result)
